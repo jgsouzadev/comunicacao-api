5bab9816d2586fbdf1784ad5e830650973cd5336
Joao Guilherme de Souza <joao.souza@fcamara.com.br>
2021-08-23 09:31:35 -0300

    Project Setup

---
+++ src/main/java/luizalabs/comunicacao/api/ApiComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package luizalabs.comunicacao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComunicacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luizalabs/comunicacao/api/ApiComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package luizalabs.comunicacao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

dc1457ae403aad702fcae126a9ea9dc4442e25ab
Joao Guilherme de Souza <joao.souza@fcamara.com.br>
2021-08-23 14:54:50 -0300

    feature(*): add novo endpoint

--- src/main/java/luizalabs/comunicacao/api/presentation/controller/BaseController.java
+++ src/main/java/luizalabs/comunicacao/api/presentation/controller/BaseController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
 import luizalabs.comunicacao.api.infra.service.IAgendamentoService;
+import luizalabs.comunicacao.api.infra.service.IConsultarAgendamentoService;
 
 @AllArgsConstructor
-@RequestMapping("/api/")
+@RequestMapping("/api/comunicacao")
 public class BaseController {
 
 	protected IAgendamentoService agendamentoService;
+	protected IConsultarAgendamentoService consultarService;
 	
 }
--- src/main/java/luizalabs/comunicacao/api/presentation/controller/ComunicacaoController.java
+++ src/main/java/luizalabs/comunicacao/api/presentation/controller/ComunicacaoController.java
@@ -1,24 +1,34 @@
 package luizalabs.comunicacao.api.presentation.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import luizalabs.comunicacao.api.domain.model.Comunicacao;
 import luizalabs.comunicacao.api.infra.dto.ComunicacaoDTO;
 import luizalabs.comunicacao.api.infra.service.IAgendamentoService;
+import luizalabs.comunicacao.api.infra.service.IConsultarAgendamentoService;
 
 @RestController
 public class ComunicacaoController extends BaseController {
 
-	public ComunicacaoController(IAgendamentoService agendamentoService) {
-		super(agendamentoService);
+	public ComunicacaoController(IAgendamentoService agendamentoService, IConsultarAgendamentoService consultarService) {
+		super(agendamentoService, consultarService);
 	}
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return agendamentoService.helloWorld();
 	}
+	
+	@GetMapping("/{id}")
+	public Comunicacao recuperarComunicacao(@PathVariable Long id) {
+		return consultarService.consultarAgendamento(id);
+	};
 	
 	@PostMapping("/store")
 	public Comunicacao agendarEnvioMensagem(ComunicacaoDTO comunicacao) throws Exception {
